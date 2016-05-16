aeb9349722394cb29c0948824f47f688516399a8
confidence68 <confidence68@126.com>
2016-05-16 15:49:05 +0800

    mysql test

---
+++ mysql_test/dbs.js
@@ -0,0 +1,45 @@
+const http = require("http");
+const work = require("./lib/timetrack");
+const mysql = require("mysql");
+
+const db = mysql.createConnection({
+	host: "127.0.0.1",
+	user: "root",
+	password: "",
+	database: "timetrack"
+})
+
+const server = http.createServer((req, res) => {
+	switch (req.method) {
+		case "POST":
+			switch (req.url) {
+				case "/":
+					work.add(db, req, res);
+					break;
+				case "/archive":
+					work.archive(db, req, res);
+					break;
+				case "/delete":
+					work.delete(db, req, res);
+					break;
+			}
+			break;
+		case "GET":
+			switch (req.url) {
+				case "/":
+					work.show(db, res);
+					break;
+				case "/archived":
+					work.showArchived(db, res);
+					break;
+			}
+			break;
+	}
+});
+
+db.query(
+	"CREATE TABLE IF NOT EXISTS work (id INT(10) NOT NULL AUTO_INCREMENT, hours DECIMAL(5,2) DEFAULT 0,date DATE,archived INT(1) DEFAULT 0, description LONGTEXT,PRIMARY KEY(id))", (err) => {
+		if (err) throw err;
+		console.log("server started...");
+		server.listen(3000, "127.0.0.1");
+	})
